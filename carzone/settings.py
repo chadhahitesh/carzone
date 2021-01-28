0c91e68523bc12393922f7508001ed1089796416
Hitesh <chadha.hitesh@gmail.com>
2021-01-28 23:01:12 +0530

    Completed Teams Module

--- carzone/settings.py
+++ carzone/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'kundan',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -124,3 +128,8 @@ STATIC_ROOT= BASE_DIR/'static'
 STATICFILES_DIRS= [
  BASE_DIR/'carzone/static',
 ]
+
+## Media settings
+
+MEDIA_ROOT = BASE_DIR/'media'
+STATIC_URL = '/media/'
